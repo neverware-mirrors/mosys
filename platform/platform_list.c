e4043460d15941d2aaf1b6201657ed5eced42b64
Kan Yan <kyan@google.com>
2015-09-01 15:17:36 -0700

    Cyclone: Add support for Cyclone.
    Based on storm.
    
    BUG=chrome-os-partner:44345
    TEST="mosys platform name" successfully identifies Cyclone.
    TEST="crossystem" no longer showing error due to mosys.
    BRANCH=None
    
    Change-Id: I9435879f98c086135302415ee6c40baa8e39c501
    Signed-off-by: Kan Yan <kyan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/297612
    Commit-Ready: Kan Yan <kyan@google.com>
    Tested-by: Kan Yan <kyan@google.com>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/319840
    Commit-Queue: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: Chun-ta Lin <itspeter@chromium.org>

--- platform/platform_list.c
+++ platform/platform_list.c
@@ -35,6 +35,7 @@
 
 /* experimental */
 extern struct platform_intf platform_beltino;
+extern struct platform_intf platform_cyclone;
 extern struct platform_intf platform_kiev;
 extern struct platform_intf platform_nyan;
 extern struct platform_intf platform_rambi;
@@ -117,6 +118,9 @@ struct platform_intf *platform_intf_list[] = {
 /* experimental platforms */
 #ifdef CONFIG_EXPERIMENTAL_BELTINO
 	&platform_beltino,
+#endif
+#ifdef CONFIG_PLATFORM_CYCLONE
+	&platform_cyclone,
 #endif
 #ifdef CONFIG_EXPERIMENTAL_KIEV
 	&platform_kiev,
