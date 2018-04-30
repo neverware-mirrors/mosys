c30a09a7f8e6b3ddcb65a4400540a1d7e47eef1f
Brian Norris <briannorris@chromium.org>
2018-04-30 10:56:58 -0700

    file: fix warnings
    ../mosys-9999/lib/file/file.c:127:10: error: format specifies type 'unsigned long' but the argument has type '__ino64_t' (aka 'unsigned long long') [-Werror,-Wformat]
                            file, st1.st_ino, st2.st_ino);
                                  ^~~~~~~~~~
    ../mosys-9999/lib/file/file.c:127:22: error: format specifies type 'unsigned long' but the argument has type '__ino64_t' (aka 'unsigned long long') [-Werror,-Wformat]
                            file, st1.st_ino, st2.st_ino);
                                              ^~~~~~~~~~
    ../mosys-9999/lib/file/file.c:143:10: error: format specifies type 'unsigned long' but the argument has type '__nlink_t' (aka 'unsigned int') [-Werror,-Wformat]
                            file, st2.st_nlink);
                                  ^~~~~~~~~~~~
    
    BUG=chromium:838349
    TEST=build
    
    Change-Id: Ic22c88015570d6cc1662c7634e53a837736731a9
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1036239
    Reviewed-by: Jason Clinton <jclinton@chromium.org>

--- lib/file/file.c
+++ lib/file/file.c
@@ -123,8 +123,8 @@ int file_open(const char *file, int rw)
 
 	/* verify inode */
 	if (st1.st_ino != st2.st_ino) {
-		lprintf(LOG_NOTICE, "File %s has invalid inode: %lx != %lx\n",
-			file, st1.st_ino, st2.st_ino);
+		lprintf(LOG_NOTICE, "File %s has invalid inode: %jx != %jx\n",
+			file, (uintmax_t)st1.st_ino, (uintmax_t)st2.st_ino);
 		close(fd);
 		return -1;
 	}
@@ -139,8 +139,8 @@ int file_open(const char *file, int rw)
 
 	/* verify inode */
 	if (st2.st_nlink != 1) {
-		lprintf(LOG_NOTICE, "File %s has invalid link count: %lx != 1\n",
-			file, st2.st_nlink);
+		lprintf(LOG_NOTICE, "File %s has invalid link count: %d != 1\n",
+			file, (int)st2.st_nlink);
 		close(fd);
 		return -1;
 	}
