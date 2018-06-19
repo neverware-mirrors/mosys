9876b939610d167ddde7eb522dbe9372cba0d6ce
Simon Glass <sjg@chromium.org>
2018-06-19 15:31:09 -0600

    cros_config_struct: Add logging for the x86 case
    At present this code silently succeeds or fails. It is not very helpful
    for people trying to figure out what is wrong.
    
    Add some logging to match the FDT code. These messages will appear when
    using 'mosys -vvvv'.
    
    BUG=b:110376187
    TEST=FEATURES=test emerge-grunt --nodeps mosys
    
    Change-Id: I5750d01f2354adcb6bcc9d73f624b13b7b1c94c0
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1107004
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- lib/cros_config/cros_config_struct.c
+++ lib/cros_config/cros_config_struct.c
@@ -15,6 +15,11 @@
 #include "lib/smbios.h"
 
 #ifdef CONFIG_PLATFORM_ARCH_X86
+static const char *str_or_null(const char *in_str)
+{
+	return in_str ?: "(null)";
+}
+
 int cros_config_read_sku_info_struct(struct platform_intf *intf,
 				     const char *smbios_name, int sku_id,
 				     struct sku_info *sku_info)
@@ -27,26 +32,39 @@ int cros_config_read_sku_info_struct(struct platform_intf *intf,
 	int config_map_size = 0;
 	const struct config_map *configs =
 	    cros_config_get_config_map(&config_map_size);
+
+	lprintf(LOG_DEBUG, "%s: Checking config: smbios_name='%s', sku_id=%d, whitelabel_tag='%s', customization_id='%s'\n",
+		__func__, str_or_null(smbios_name), sku_id,
+		str_or_null(whitelabel_tag), str_or_null(customization_id));
 	for (int i = 0; i < config_map_size; i++) {
 		const struct config_map *config = &configs[i];
-		bool smbios_match =
-		    !strcmp("", config->smbios_match_name) ||
+
+		lprintf(LOG_DEBUG, "   item %d: smbios_match_name='%s', sku_id=%d, whitelabel_tag='%s', customization_id='%s'\n",
+			i, config->smbios_match_name, config->sku_id,
+			config->whitelabel_tag, config->customization_id);
+		bool smbios_match = !*config->smbios_match_name ||
 		    !strcmp(smbios_name, config->smbios_match_name);
 		bool sku_match =
 		    (-1 == config->sku_id) || (sku_id == config->sku_id);
 		bool whitelabel_match =
-		    !strcmp("", config->whitelabel_tag) ||
+		    !*config->whitelabel_tag ||
 		    !strcmp(whitelabel_tag, config->whitelabel_tag);
 		bool customization_match =
-		    !strcmp("", config->customization_id) ||
+		    !*config->customization_id ||
 		    !strcmp(customization_id, config->customization_id);
 		if (smbios_match && sku_match && customization_match &&
 		    whitelabel_match) {
 			intf->name = config->platform_name;
 			memcpy(sku_info, &config->info, sizeof(*sku_info));
+			lprintf(LOG_DEBUG, "   - Found, name='%s', model='%s', brand='%s'\n",
+				str_or_null(intf->name),
+				str_or_null(sku_info->model),
+				str_or_null(sku_info->brand));
+
 			return 0;
 		}
 	}
+	lprintf(LOG_DEBUG, "   - Not found\n");
 
 	return -1;
 }
