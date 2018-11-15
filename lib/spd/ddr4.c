5fe72274214c3b9a8063cee64878ba9a5f9d5244
Cheng-Han Yang <chenghan@google.com>
2018-11-15 18:59:14 +0800

    mosys: Increase part number string size.
    BUG=b:119535940
    TEST=Manually test on DUT.
         Check that mosys output matches vendor part number.
    
    Change-Id: I7fa6c2eaacf1b7deb57f06465ca94fc3068cabd2
    Reviewed-on: https://chromium-review.googlesource.com/c/1337350
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>
    (cherry picked from commit 002ad2f5add7e750f2fa303af59fbf0afd79706b)
    Reviewed-on: https://chromium-review.googlesource.com/1360099
    Commit-Ready: David Wu <david_wu@quanta.corp-partner.google.com>
    Tested-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-by: Cheng-Han Yang <chenghan@chromium.org>
    Reviewed-by: Peter Hung <peter.hung@quanta.corp-partner.google.com>
    (cherry picked from commit 7be574e132c84bc9fc394a77b7ac6afb17dfd315)
    Reviewed-on: https://chromium-review.googlesource.com/c/1376953
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>

--- lib/spd/ddr4.c
+++ lib/spd/ddr4.c
@@ -158,10 +158,10 @@ int spd_print_field_ddr4(struct platform_intf *intf, struct kv_pair *kv,
 
 	case SPD_GET_PART_NUMBER:
 	{
-		char part[19];
+		char part[20];
 
-		memcpy(part, &byte[DDR4_SPD_REG_MODULE_PART_NUM_0], 18);
-		part[18] = '\0';
+		memcpy(part, &byte[DDR4_SPD_REG_MODULE_PART_NUM_0], 19);
+		part[19] = '\0';
 		kv_pair_fmt(kv, "part_number", "%s", part);
 
 		ret = 1;
