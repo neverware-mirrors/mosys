6524373582b34e1f2ef89c2f8026ffc6f61fddd3
Simon Glass <sjg@chromium.org>
2019-07-02 16:37:49 -0600

    Drop spring which has hit AUE (auto-update end)
    This board hit AUE in M72 so can be removed.
    
    BUG=chromium:980752
    TEST=emerge-sarien sys-apps/mosys
    
    Change-Id: I191c1c4d40255ca035e763819079b9b688271385
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1686423
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>

--- platform/platform_list.c
+++ platform/platform_list.c
@@ -65,7 +65,6 @@ extern struct platform_intf platform_samus;
 extern struct platform_intf platform_sarien;
 extern struct platform_intf platform_skate;
 extern struct platform_intf platform_slippy;
-extern struct platform_intf platform_spring;
 extern struct platform_intf platform_storm;
 extern struct platform_intf platform_strago;
 
@@ -106,7 +105,6 @@ struct platform_intf *platform_intf_list[] = {
 	&platform_peach,
 	&platform_pinky,
 	&platform_skate,
-	&platform_spring,
 	&platform_storm,
 #ifdef CONFIG_CROS_CONFIG
 	&platform_cheza,
