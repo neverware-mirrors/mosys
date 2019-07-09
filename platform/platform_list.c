f381336c9fa538e5f134c1535c0194481228ee0f
Simon Glass <sjg@chromium.org>
2019-07-09 14:44:15 -0600

    Drop dragonegg which was never launched
    This board was never shipped so can be removed.
    
    BUG=chromium:990438
    TEST=emerge-sarien sys-apps/mosys
    
    Change-Id: Icec5a69d91ee645ff3595f8cbc6a51a1b7279f99
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/1746498
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@google.com>
    Commit-Queue: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>

--- platform/platform_list.c
+++ platform/platform_list.c
@@ -43,7 +43,6 @@ extern struct platform_intf platform_cheza;
 extern struct platform_intf platform_coral;
 extern struct platform_intf platform_cyan;
 extern struct platform_intf platform_cyclone;
-extern struct platform_intf platform_dragonegg;
 extern struct platform_intf platform_fizz;
 extern struct platform_intf platform_flapjack;
 extern struct platform_intf platform_glados;
@@ -73,7 +72,6 @@ struct platform_intf *platform_intf_list[] = {
 	&platform_beltino,
 	&platform_coral,
 	&platform_cyan,
-	&platform_dragonegg,
 	&platform_glados,
 	&platform_rambi,
 	&platform_reef,
