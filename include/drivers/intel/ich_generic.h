8b89f457512390cd776b2b52cc9013732d2f2acb
Simon Glass <sjg@chromium.org>
2019-09-26 14:15:18 -0600

    Drop unused ich_gpio file and ich_generic functions
    The ich_gpio file is not used anymore, so drop it.
    
    The ich_generic C file is not used and most of the ich_generic header file
    is not needed. Drop it.
    
    BUG=chromium:990438
    TEST=emerge-sarien -q mosys
    
    Change-Id: I7d0f21968f3f43bf546d1477c70d847d0077b428
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/1830318
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Commit-Queue: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- include/drivers/intel/ich_generic.h
+++ include/drivers/intel/ich_generic.h
@@ -32,14 +32,6 @@
 #ifndef MOSYS_DRIVERS_INTEL_ICH_H__
 #define MOSYS_DRIVERS_INTEL_ICH_H__
 
-/* convert GPIO# to pin# */
-#define ICH_GPIO_PORT1_TO_PIN(x)	(32 - (x))
-#define ICH_GPIO_PORT2_TO_PIN(x)	(64 - (x))
-
-/* forward declarations */
-struct platform_intf;
-struct gpio_map;
-
 enum ich_generation {
 	ICH7,		/* ICH7 and NM10 */
 	ICH8,
@@ -50,113 +42,5 @@ enum ich_generation {
 	ICH_8_SERIES,	/* LynxPoint-H */
 	ICH_8_LPSS,	/* LynxPoint-LP */
 };
-
-/* Boot BIOS Straps for ICH7-ICH10 chipsets */
-enum ich_bbs_ich7 {
-	ICH7_BBS_UNKNOWN	= -1,
-	ICH7_BBS_RSVD		= 0x0,	/* note: this is also SPI on ICH10 */
-	ICH7_BBS_SPI		= 0x1,
-	ICH7_BBS_PCI		= 0x2,
-	ICH7_BBS_LPC		= 0x3,
-};
-
-/* Boot BIOS Straps for Sandy Bridge and newer chipsets */
-enum ich_snb_bbs {
-	ICH_SNB_BBS_UNKNOWN	= -1,
-	ICH_SNB_BBS_LPC		= 0x0,
-	ICH_SNB_BBS_RSVD	= 0x1,
-	ICH_SNB_BBS_PCI		= 0x2,
-	ICH_SNB_BBS_SPI		= 0x3,
-};
-
-/*
- * ich_get_bbs - get bios boot straps (bbs) value
- *
- * @intf:	platform interface
- *
- * returns BBS value to indicate success
- * returns <0 to indicate failure
- */
-extern int ich_get_bbs(struct platform_intf *intf);
-
-/*
- * ich_set_bbs - set bios boot straps (bbs) value
- *
- * @intf:	platform interface
- * @bbs:	bbs value
- *
- * returns 0 to indicate success
- * returns <0 to indicate failure
- */
-int ich_set_bbs(struct platform_intf *intf, int bbs);
-
-
-/*
- * ich_get_gpio_base  - get GPIO base address.
- *
- * @intf:	platform interface
- * @val:	location to store value
- *
- * This function is primarily useful when setting values that are related to
- * GPIOs. For reading and writing GPIOs themselves, use the ich_read_gpio and
- * ich_set_gpio wrapper functions for added safety.
- * 
- * returns 0 to indicate success
- * returns <0 on read failure
- */
-extern int ich_get_gpio_base(struct platform_intf *intf, uint16_t *val);
-
-/*
- * ich_read_gpio  - read GPIO status
- *
- * @intf:	platform interface
- * @gen:	chipset generation
- * @gpio:	gpio map
- *
- * returns GPIO state as 0 or 1
- * returns <0 on read failure
- */
-int ich_read_gpio(struct platform_intf *intf,
-                  enum ich_generation gen, struct gpio_map *gpio);
-
-/*
- * ich_set_gpio  - set GPIO status
- *
- * @intf:	platform interface
- * @gen:	chipset generation
- * @gpio:	gpio map
- * @status:	0/1
- *
- * returns 0 if successful
- * returns <0 on read failure
- */
-int ich_set_gpio(struct platform_intf *intf, enum ich_generation gen,
-                 struct gpio_map *gpio, int state);
-
-/*
- * ich_gpio_list  -  list GPIOs in a given bank
- *
- * @intf:	platform interface
- * @port:	GPIO port
- * @gpio_pins:	set of GPIO pins to list
- * @num_gpios:	number of GPIOs in set
- *
- * Note: Some chipsets do not implement certain GPIOs. Use the gpios
- * array to specify which GPIOs should be listed by default.
- *
- * returns 0 if successful
- * returns <0 if failure
- */
-extern int ich_gpio_list(struct platform_intf *intf, enum ich_generation gen,
-                         int port, int gpio_pins[], int num_gpios);
-
-/*
- * ich_global_reset - initiate reset via reset control register (0xcf9)
- *
- * @intf:	platform interface
- *
- * returns <0 to indicate failure
- */
-extern int ich_global_reset(struct platform_intf *intf);
 
 #endif /* MOSYS_DRIVERS_INTEL_ICH_H__ */
