12a66ffbe5ed558436bdf9e2e2a87a270152fa7a
Simon Glass <sjg@chromium.org>
2019-09-26 14:15:18 -0600

    Drop Intel series6 things
    This code is no-longer used and only one #define from the header file is
    needed. Drop the unused code.
    
    BUG=chromium:990438
    TEST=emerge-sarien -q mosys
    
    Change-Id: I01269c1a14903f88f3abdf87571f95a4f3630c1b
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1829836
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- include/drivers/intel/series6.h
+++ include/drivers/intel/series6.h
@@ -32,82 +32,11 @@
 #ifndef MOSYS_DRIVERS_INTEL_SERIES6_H__
 #define MOSYS_DRIVERS_INTEL_SERIES6_H__
 
-struct platform_intf;
-struct gpio_map;
-
 /*
  * The 6-Series has one SMBus interface, so for the purpose of matching with
  * sysfs entries we don't need to worry about the exact IO address which appears
  * at the end of the string (e.g. "SMBus I801 adapter at 2000").
  */
 #define SERIES6_SMBUS_ADAPTER	"SMBus I801 adapter"
-
-/*
- * series6_get_bbs - get bios boot straps (bbs) value
- *
- * @intf:	platform interface
- *
- * returns BBS value to indicate success
- * returns <0 to indicate failure
- */
-enum ich_snb_bbs series6_get_bbs(struct platform_intf *intf);
-
-/*
-  * series6_set_bbs - set bios boot straps (bbs) value
-  *
-  * @intf:	platform interface
-  * @bbs:	bbs value
-  *
-  * returns 0 to indicate success
-  * returns <0 to indicate failure
-  */
-int series6_set_bbs(struct platform_intf *intf, enum ich_snb_bbs bbs);
-
-/*
- * series6_read_gpio  - read GPIO status
- *
- * @intf:	platform interface
- * @gpio:	gpio map
- *
- * returns GPIO state as 0 or 1
- * returns <0 on read failure
- */
-
-int series6_read_gpio(struct platform_intf *intf, struct gpio_map *gpio);
-
-/*
- * series6_set_gpio  - set GPIO status
- *
- * @intf:	platform interface
- * @gpio:	gpio map
- * @status:	0/1
- *
- * returns 0 if successful
- * returns <0 on read failure
- */
-int series6_set_gpio(struct platform_intf *intf, struct gpio_map *gpio, int state);
-
-/*
- * series6_gpio_list  -  list all GPIOs and their states
- *
- * @intf:	platform interface
- *
- * returns 0 if successful
- * returns <0 if failure
- */
-extern int series6_gpio_list(struct platform_intf *intf);
-
-/*
- * series6_global_reset - force global reset
- *
- * @intf:	platform interface
- *
- * Obviously this function should force the platform to die before
- * returning, but we have a return code anyway...
- *
- * returns 0 if successful
- * returns <0 if failure
- */
-extern int series6_global_reset(struct platform_intf *intf);
 
 #endif /* MOSYS_DRIVERS_INTEL_SERIES6_H__ */
