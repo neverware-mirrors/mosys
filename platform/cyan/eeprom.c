953f909a9c8ad0b29f912f11d7831461f14a7d84
Jack Rosenthal <jrosenth@chromium.org>
2020-01-31 14:56:16 -0700

    cyan: delete unused cyan_eeprom_setup function
    Unused, delete.
    
    BUG=chromium:990438
    TEST=compiles
    
    Change-Id: I56e21ff5ed12c1a49572b9cc4100f6b880e92c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/2033697
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Auto-Submit: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- platform/cyan/eeprom.c
+++ platform/cyan/eeprom.c
@@ -85,21 +85,6 @@ static struct eeprom cyan_eeproms[] = {
 	{ 0 },
 };
 
-int cyan_eeprom_setup(struct platform_intf *intf)
-{
-	struct eeprom *eeprom;
-	int rc = 0;
-
-	for (eeprom = intf->cb->eeprom->eeprom_list;
-	     eeprom && eeprom->name;
-	     eeprom++) {
-		if (eeprom->setup)
-			rc |= eeprom->setup(intf, eeprom);
-	}
-
-	return rc;
-}
-
 struct eeprom_cb cyan_eeprom_cb = {
 	.eeprom_list	= cyan_eeproms,
 };
