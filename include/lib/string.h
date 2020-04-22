e6040d26af17e7038a59967e246ca07cea8498e7
Jack Rosenthal <jrosenth@chromium.org>
2020-04-22 11:15:10 -0600

    lib/string: remove some unused string functions
    There were a bunch of unused functions in here. In particular, some
    network card stuff?!?!  The smell of YAGNI is really strong...
    
    BUG=chromium:990438
    TEST=compiles
    
    Change-Id: I8a5b31d9a5c2eb2aaa738da77489ed43dc017ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/2161529
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- include/lib/string.h
+++ include/lib/string.h
@@ -39,8 +39,6 @@
 #include <inttypes.h>
 #include <stddef.h>
 
-/* FIXME: Should we #include string_builder.h and valstr.h here? */
-
 /*
  * convert an array of bytes to a hexadecimal string (no prefix)
  *
@@ -66,77 +64,6 @@ extern char *format_string(const char *fmt, ...)
 extern int find_pattern(void *haystack, size_t haystack_length,
                         void *needle, size_t needle_length,
                         size_t align, size_t *offset);
-
-/*
- * rshift_nibbles - Right-shift nibbles in an array by 1 (4 bits)
- *
- * @array:	array to shift
- * @len:	length of array
- */
-extern void rshift_nibbles(uint8_t array[], size_t len);
-
-/*
- * lshift_nibbles - Left-shift nibbles in an array by 1 (4 bits)
- *
- * @array:	array to shift
- * @len:	length of array
- */
-extern void lshift_nibbles(uint8_t array[], size_t len);
-
-/*
- * Return a pointer to a dynamically allocated string representing the nth
- * field, delimited by delim, in str.
- */
-extern char *strfield(const char *str, char delim, int n);
-
-/**
- * nstr2buf - convert a string of numerical characters into binary form
- *
- * @buf: 	buffer to store result
- * @str:	null-terminated string to convert
- * @base:	number base to convert to, range is 2 to 16
- * @delim:	string containing all delimiting characters
- *
- * This function will store the content of the provided string into a buffer.
- * Delimiting characters will be left out of the result. This is essentially
- * a glorified wrapper around strtoul().
- *
- * returns length of buffer if successful
- * returns <0 to indicate error
- */
-extern int nstr2buf(uint8_t **buf, const char *str,
-                    int base, const char *delim);
-
-/* Network device ID types */
-enum nic_id_type {
-	NIC_ID_UNSPECIFIED = 0,
-	NIC_ID_IEEE802,
-	NIC_ID_IMEI,
-	NIC_ID_IMEISV,
-	NIC_ID_MEID,
-};
-
-/* Network device ID lengths */
-enum {
-	NIC_ID_IEEE802_LENGTH = 6,
-	NIC_ID_IMEI_LENGTH = 8,
-	NIC_ID_IMEISV_LENGTH = 8,
-	NIC_ID_MEID_LENGTH = 7,
-};
-
- /**
- * buf2nicid - Convert raw data to a formatted, null-terminated NIC ID string
- *
- * @data:	raw data
- * @type:	type of network ID to produce from input buffer
- *
- * For IEEE802.x (MAC-48), output will be in the form "01:23:45:67:89:ab"
- * For IMEI, output will be in the form "22-666666-666666-1"
- *
- * returns an allocated network device ID string if successful
- * returns NULL to indicate failure
- */
-extern char *buf2nicid(uint8_t *inbuf, enum nic_id_type type);
 
 /*
  * strlfind - linear search for string in set of strings
@@ -169,19 +96,5 @@ char *strupper(char *str);
 
 /* From BSD-land, see notes in "man 3 strcpy" */
 size_t strlcpy(char *dest, const char *src, size_t size);
-
-/*
- * Find the min-length string.
- */
-#ifndef __minlen
-# define __minlen(a, b) ({ int x=strlen(a); int y=strlen(b); (x < y) ? x : y;})
-#endif
-
-/*
- * Find the max-length string.
- */
-#ifndef __maxlen
-# define __maxlen(a, b) ({ int x=strlen(a); int y=strlen(b); (x > y) ? x : y;})
-#endif
 
 #endif /* MOSYS_LIB_STRING_H_ */
