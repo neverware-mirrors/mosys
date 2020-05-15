c4214331a418f95de1a1c6c9f4426612550e50ae
Jack Rosenthal <jrosenth@chromium.org>
2020-05-14 21:07:53 -0600

    smbios: switch to dmi in sysfs for SMBIOS
    At the security team's request, switch to using DMI in sysfs for
    reading SMBIOS in mosys.  This is now possible that CONFIG_DMI_SYSFS
    is enabled in the kernel (crbug.com/1080674).
    
    This also will hopefully eliminate occasional crashes we are seeing in
    smbios_find_entry, since it deletes the function.  (Search
    crash.corp.google.com for a magic signature of "smbios_find_entry").
    
    BUG=chromium:1083048
    TEST=on samus:
      - mosys memory spd print all
      - mosys eventlog list
    
    Change-Id: I5b81c1fee79413db99aa76f35fdf73ea8f8ec52d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/2203725
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Victor Ding <victording@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- lib/smbios/smbios.c
+++ lib/smbios/smbios.c
@@ -28,37 +28,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <linux/limits.h>
+#include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/klog.h>
-#include <fcntl.h>
-#include <ctype.h>
-
-#include "mosys/alloc.h"
-#include "mosys/globals.h"
-#include "mosys/log.h"
-#include "mosys/platform.h"
-#include "mosys/output.h"
-
-#include "intf/mmio.h"
 
 #include "lib/file.h"
 #include "lib/smbios.h"
-#include "lib/string.h"
-
-/* Iterator used for table parsing */
-struct smbios_iterator {
-	struct smbios_entry *entry;	/* entry pointer */
-	struct smbios_header *header;	/* current header */
-	uint8_t *data;			/* table data */
-	uint8_t *current;		/* pointer into data */
-};
-
-static struct smbios_iterator *smbios_itr = NULL;
+#include "mosys/log.h"
+#include "mosys/platform.h"
 
 /*
  * smbios_parse_string_table  -  parse strings into table structure
@@ -104,336 +83,6 @@ static int smbios_parse_string_table(
 }
 
 /*
- * smbios_string_table_len -  find length of smbios string table
- *
- * @ptr:	pointer to start of strings
- *
- * returns offset to end of string list (including terminating null byte).
- * returns < 0 if ptr is invalid.
- */
-static int smbios_string_table_len(const char *ptr)
-{
-	int len, tlen;
-
-	if (ptr == NULL)
-		return -1;
-
-	/* empty string list */
-	if (!*ptr)
-		return 2;
-
-	/* count each string length */
-	for (tlen = 1; ptr && *ptr; ptr += len) {
-		len = strlen(ptr) + 1;
-		tlen += len;
-	}
-
-	return tlen;
-}
-
-/*
- * smbios_find_entry  -  locate entry pointer in EBDA
- *
- * @intf:	platform interface
- * @entry:	buffer to store entry pointer must be allocated by caller
- * @baseaddr:	address to begin search
- * @len:	length of region to search (in bytes)
- *
- * returns 0 if found
- * returns <0 if not found
- */
-static int smbios_find_entry(struct platform_intf *intf,
-			     struct smbios_entry *entry,
-			     unsigned long int baseaddr, unsigned long int len)
-{
-	uint8_t csum;
-	uint8_t *data;
-	size_t offset;
-	int i;
-	uint8_t smbios_magic[] = SMBIOS_ENTRY_MAGIC;
-	int found = 0;
-
-	data = mmio_map(intf, O_RDONLY, baseaddr, len);
-	if (data == NULL) {
-		lprintf(LOG_DEBUG, "Unable to map SMBIOS entry buffer.\n");
-		return -1;
-	}
-
-	if (find_pattern(data, len,
-	                 &smbios_magic[0], 4, 16, &offset) < 0) {
-		mmio_unmap(intf, data, baseaddr, len);
-		found = 0;
-	} else {
-		found = 1;
-	}
-
-	/*
-	 * Some systems with the right patches will export the SMBIOS base
-	 * to debugfs.
-	 */
-	if (!found) {
-		FILE *fp;
-		char line[LINE_MAX];
-
-		lprintf(LOG_DEBUG, "%s: Attempting to get SMBIOS base address "
-		"from /sys/kernel/debug/efi_smbios_base\n", __func__);
-
-		/* ChromeOS currently exports smbios base in debugfs. */
-		fp = fopen("/sys/kernel/debug/efi_smbios_base", "r");
-		if ((fp > 0) && (fgets(&line[0], LINE_MAX, fp) != NULL)) {
-			baseaddr = strtoull(line, NULL, 0);
-			lprintf(LOG_DEBUG, "%s: SMBIOS=0x%08lx\n",
-			                   __func__, baseaddr);
-			data = mmio_map(intf, O_RDONLY, baseaddr, len);
-			offset = 0;
-			found = 1;
-			fclose(fp);
-		}
-	}
-
-	if (!found) {
-		char *buf;
-		int klog_size;
-		size_t klog_offset;
-
-		/*
-		 * Try parsing kernel log for SMBIOS= address. This is useful
-		 * in situations where the SMBIOS address is out of spec and not
-		 * easy to find from userspace (possibly due to EFI).
-		 */
-		lprintf(LOG_DEBUG, "%s: Attempting to get SMBIOS base address "
-		                   "from kernel log\n", __func__);
-
-		if ((klog_size = klogctl(10, NULL, 0)) < 0)
-			return -1;
-
-		buf = malloc(klog_size);
-		if ((klogctl(3, buf, klog_size)) < 0) {
-			lprintf(LOG_DEBUG, "Unable to read kernel log\n");
-		} else {
-			if (find_pattern(buf, klog_size,
-			                 "SMBIOS=", 7, 1, &klog_offset) == 0) {
-				baseaddr = strtoull(buf + klog_offset + 7,
-				                    NULL, 0);
-				lprintf(LOG_DEBUG, "kernel log offset: %zx, "
-				                   "SMBIOS=0x%08lx\n",
-					           klog_offset, baseaddr);
-				data = mmio_map(intf, O_RDONLY, baseaddr, len);
-				offset = 0;
-				found = 1;
-			} else {
-				lprintf(LOG_DEBUG, "SMBIOS entry point not"
-				                   "found in kernel log\n");
-			}
-		}
-
-		free(buf);
-	}
-
-	if (!found) {
-		lprintf(LOG_DEBUG, "Unable to find SMBIOS entry.\n");
-		return -1;
-	}
-
-
-	lprintf(LOG_DEBUG, "SMBIOS Table Entry @ 0x%lx\n", baseaddr + offset);
-
-	/* copy entry into user-provided buffer */
-	memset(entry, 0, sizeof(*entry));
-	memcpy(entry, data + offset, sizeof(*entry));
-
-	/* verify entry pointer checksum */
-	for (csum = i = 0; i < entry->entry_length; i++)
-		csum += data[offset + i];
-
-	mmio_unmap(intf, data, baseaddr, len);
-
-	if (csum != 0) {
-		lprintf(LOG_DEBUG, "Invalid SMBIOS checksum: %02x\n", csum);
-	}
-
-	lprintf(LOG_DEBUG, "SMBIOS Entry Version:   %d.%d\n",
-	        entry->major_ver, entry->minor_ver);
-	lprintf(LOG_DEBUG, "SMBIOS Table Address:   0x%08x\n",
-	        entry->table_address);
-	lprintf(LOG_DEBUG, "SMBIOS Table Length:    %d\n",
-	        entry->table_length);
-	lprintf(LOG_DEBUG, "SMBIOS Table Count:     %d\n",
-	        entry->table_entry_count);
-
-	return 0;
-}
-
-/*
- * smbios_itr_destroy  -  clean up iterator
- *
- * @intf:	platform interface
- */
-static void smbios_itr_destroy(void *arg)
-{
-	struct platform_intf *intf = arg;
-	if (smbios_itr) {
-		/* clean up smbios table buffer */
-		if (smbios_itr->data) {
-			mmio_unmap(intf, smbios_itr->data,
-			           smbios_itr->entry->table_address,
-			           smbios_itr->entry->table_length);
-		}
-		/* clean up smbios entry */
-		if (smbios_itr->entry)
-			free(smbios_itr->entry);
-		/* clean up iterator */
-		free(smbios_itr);
-		smbios_itr = NULL;
-	}
-}
-
-/*
- * smbios_itr_setup  -  setup iterator for smbios searching
- *
- * @intf:	platform interface
- * @baseaddr:	base address to start searching in
- * @len:	length of region to search (in bytes)
- *
- * returns 0 to indicate successful setup or reset to setup
- * returns <0 to indicate failure
- */
-static int smbios_itr_setup(struct platform_intf *intf,
-                            unsigned int baseaddr, unsigned int len)
-{
-	/* already setup? */
-	if (smbios_itr) {
-		/* reset to start of tables */
-		smbios_itr->current = smbios_itr->data;
-		smbios_itr->header =
-		        (struct smbios_header *)smbios_itr->current;
-		return 0;
-	}
-
-	/*
-	 * Setup iterator
-	 *
-	 * TODO(crbug.com/1018847): Fix leak of smbios_itr and
-	 * smbios_itr->entry
-	 */
-	smbios_itr = mosys_malloc(sizeof(*smbios_itr));
-	memset(smbios_itr, 0, sizeof(*smbios_itr));
-
-	smbios_itr->entry = mosys_malloc(sizeof(*smbios_itr->entry));
-	memset(smbios_itr->entry, 0, sizeof(*smbios_itr->entry));
-
-	/* search for entry pointer */
-	if (smbios_find_entry(intf, smbios_itr->entry, baseaddr, len) < 0) {
-		smbios_itr_destroy(intf);
-		return -1;
-	}
-	if (smbios_itr->entry->table_length == 0) {
-		smbios_itr_destroy(intf);
-		return -1;
-	}
-
-	/* mmap in entire smbios area */
-	smbios_itr->data = mmio_map(intf, O_RDONLY,
-	                            smbios_itr->entry->table_address,
-	                            smbios_itr->entry->table_length);
-
-	if (smbios_itr->data == NULL) {
-		lprintf(LOG_ERR, "Unable to find SMBIOS tables at 0x%08x\n",
-		        smbios_itr->entry->table_address);
-		smbios_itr_destroy(intf);
-		return -1;
-	}
-
-	if (mosys_get_verbosity() == LOG_DEBUG)
-		print_buffer(smbios_itr->data, smbios_itr->entry->table_length);
-
-	/* start pointer at table start */
-	smbios_itr->current = smbios_itr->data;
-	smbios_itr->header = (struct smbios_header *)smbios_itr->current;
-
-	return 0;
-}
-
-/*
- * smbios_itr_next  -  find next table from current iterator
- *
- * @intf	platform interface
- *
- * returns 0 if found
- * returns 1 if search wrapped to start
- * returns <0 if iterator not setup or invalid
- */
-static int smbios_itr_next(struct platform_intf *intf)
-{
-	int rc = 0;
-
-	if (!smbios_itr || !smbios_itr->current || !smbios_itr->header)
-		return -1;
-
-	/* adjust pointer to end of table data + strings */
-	smbios_itr->current += smbios_itr->header->length +
-		smbios_string_table_len((char *)smbios_itr->current +
-		                        smbios_itr->header->length);
-
-	/* check for overflow */
-	if (smbios_itr->current >= (smbios_itr->data +
-				    smbios_itr->entry->table_length - 1)) {
-		/* start pointer over */
-		lprintf(LOG_DEBUG, "smbios_itr_next: search wrapped\n");
-		smbios_itr->current = smbios_itr->data;
-		rc = 1;
-	}
-
-	/* adjust header pointer */
-	smbios_itr->header = (struct smbios_header *)smbios_itr->current;
-
-	return rc;
-}
-
-/*
- * smbios_find_table_raw  -  locate table and store in buffer
- *
- * @intf:	platform interface
- * @type:	smbios table type
- * @instance:	smbios table instance
- * @baseaddr:	base address to start searching in
- * @len:	length of region to search (in bytes)
- *
- * returns 0 if found
- * returns <0 if not found
- */
-static int smbios_find_table_raw(struct platform_intf *intf,
-                                 enum smbios_types type, int instance,
-                                 unsigned int baseaddr, unsigned int len)
-{
-	int ret = -1;
-
-	/* start iterator */
-	if (smbios_itr_setup(intf, baseaddr, len) >= 0) {
-		do {
-			/* check for correct type */
-			if (smbios_itr->header->type != type)
-				continue;
-
-			/* check for correct instance */
-			if (instance-- > 0)
-				continue;
-
-			if (mosys_get_verbosity() == LOG_DEBUG)
-				print_buffer(smbios_itr->current,
-				             smbios_itr->header->length);
-
-			/* found */
-			ret = 0;
-			break;
-
-		} while (smbios_itr_next(intf) == 0);
-	}
-
-	return ret;
-}
-
-/*
  * smbios_find_table  -  locate specified SMBIOS table in memory
  *
  * @intf:	platform interface
@@ -447,29 +96,19 @@ static int smbios_find_table_raw(struct platform_intf *intf,
 int smbios_find_table(struct platform_intf *intf, enum smbios_types type,
 		      int instance, struct smbios_table *table)
 {
+	char filepath[PATH_MAX];
+
 	if (!table || type > SMBIOS_TYPE_END)
 		return -1;
 
-	/* get the table as raw buffer */
-	if (smbios_find_table_raw(intf, type, instance,
-				  SMBIOS_LEGACY_ENTRY_BASE,
-				  SMBIOS_LEGACY_ENTRY_LEN) < 0) {
-		lprintf(LOG_DEBUG, "Unable to locate table %d:%d\n",
-		        type, instance);
+	snprintf(filepath, sizeof(filepath),
+		 "/sys/firmware/dmi/entries/%d-%d/raw", type, instance);
+
+	if (read_file(filepath, (char *)table, sizeof(*table), LOG_DEBUG) < 0)
 		return -1;
-	}
 
-	/* copy header first */
-	memset(table, 0, sizeof(*table));
-	memcpy(&table->header, smbios_itr->header, sizeof(table->header));
-
-	/* then table data */
-	memcpy(&table->data.data, smbios_itr->current + sizeof(table->header),
-	       smbios_itr->header->length - sizeof(table->header));
-
-	/* finally parse table strings */
-	smbios_parse_string_table((char *)smbios_itr->current +
-	                          smbios_itr->header->length, table->string);
+	smbios_parse_string_table((char *)table + table->header.length,
+				  table->string);
 
 	return 0;
 }
