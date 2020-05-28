83419db02b92dd894c399dd0f462138477ea8d7c
Jack Rosenthal <jrosenth@chromium.org>
2020-05-28 11:55:39 -0600

    platform/arm/mistral: refactor memory config to use vpd from sysfs
    Currently, this uses VPD by reading directly from flashrom, and gets
    called 6 times during a "mosys memory spd print all", which is
    certainly not fast.
    
    We have VPD in sysfs for this device, so just read the dram_id from
    there.
    
    BUG=chromium:1087454
    TEST=deploy on mistral, "mosys memory spd print all" is equivalent
         output but much faster
    
    Change-Id: I7f2b1f049a27aaf2efd72450547a18d3e23b8802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/2219926
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- platform/arm/mistral/memory.c
+++ platform/arm/mistral/memory.c
@@ -29,14 +29,12 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <ctype.h>
+#include <stdbool.h>
+#include <stddef.h>
 
-#include "lib/fdt.h"
-#include "lib/flashrom.h"
+#include "lib/file.h"
+#include "lib/math.h"
 #include "lib/nonspd.h"
-#include "lib/string.h"
-
-#include "mosys/alloc.h"
 #include "mosys/log.h"
 #include "mosys/platform.h"
 
@@ -45,19 +43,7 @@
 /* Treat each module as a logical "DIMM" */
 #define MISTRAL_DIMM_COUNT	2
 
-#define MISTRAL_RO_VPD_SIZE	0x20000
 #define MISTRAL_VPD_ENTRY_VALUE_SIZE	64
-
-enum mistral_memory_config {
-	SAMSUNG_DDR3L_1866_512M,
-	NANYA_DDR3L_1866_512M,
-	HYNIX_DDR3L_1866_512M,
-	MEM_UNKNOWN,
-};
-
-enum mistral_board_id {
-	BOARD_ID_MISTRAL_PROTO = 0, /* 000 */
-};
 
 /*
  * dimm_count  -  return total number of dimm slots
@@ -71,81 +57,42 @@ static int dimm_count(struct platform_intf *intf)
 	return MISTRAL_DIMM_COUNT;
 }
 
-/* Parse vpd partition and get value of key */
-static int parse_vpd(uint8_t *buf, size_t size, char *key,
-		     char value[MISTRAL_VPD_ENTRY_VALUE_SIZE])
+const static struct {
+	const char *part_num;
+	const struct nonspd_mem_info *info;
+} memory_configurations[] = {
+	{ "SAMSUNG_K4B4G1646E-BYMA", &samsung_k4b4g1646e_byma },
+	{ "NANYA_NT5CC256M16ER-EK", &nanya_ddr3l_nt5cc256m16er_ek },
+	{ "HYNIX_H5TC4G63EFR-RDA", &hynix_ddr3l_h5tc4g63efr_rda },
+};
+
+static int get_mem_info(struct platform_intf *intf,
+			const struct nonspd_mem_info **info_out)
 {
-	size_t offset = 0;
-	int i = 0;
+	static bool first_run = true;
+	static const struct nonspd_mem_info *info;
+	static char part_num[MISTRAL_VPD_ENTRY_VALUE_SIZE];
 
-	/* find vpd entry */
-	if (find_pattern(buf, size, key, strlen(key), 1, &offset) < 0) {
-		lprintf(LOG_ERR, "Unable to find key %s in vpd\n", key);
+	if (!first_run)
+		goto exit;
+	first_run = false;
+
+	if (read_file("/sys/firmware/vpd/ro/dram_id", part_num,
+		      sizeof(part_num), LOG_ERR) < 0)
 		return -1;
-	}
 
-	/* skip key */
-	offset += strlen(key) + 1;
-
-	for (i = 0; i < MISTRAL_VPD_ENTRY_VALUE_SIZE; ++i) {
-		if (isprint(buf[offset + i])) {
-			value[i] = buf[offset + i];
-		} else {
+	for (size_t i = 0; i < ARRAY_SIZE(memory_configurations); i++) {
+		if (!strcmp(part_num, memory_configurations[i].part_num)) {
+			info = memory_configurations[i].info;
 			break;
 		}
 	}
 
-  return 0;
-}
-
-static enum mistral_memory_config get_memory_config(struct platform_intf *intf)
-{
-	uint8_t *buf = NULL;
-	int ret = 0;
-	char memory[MISTRAL_VPD_ENTRY_VALUE_SIZE] = {0};
-	enum mistral_memory_config memory_config = MEM_UNKNOWN;
-
-	ret = flashrom_read_by_name(&buf, HOST_FIRMWARE, "RO_VPD");
-	if (ret != MISTRAL_RO_VPD_SIZE) {
-		lprintf(LOG_ERR, "Read RO_VPD error, ret = %d\n", ret);
-		goto exit;
-	}
-
-	if (parse_vpd(buf, MISTRAL_RO_VPD_SIZE, "dram_id", memory) < 0) {
-		lprintf(LOG_ERR, "Unable to determine memory configuration\n");
-		goto exit;
-	}
-
-	if (!strcmp(memory, "SAMSUNG_K4B4G1646E-BYMA")) {
-		memory_config = SAMSUNG_DDR3L_1866_512M;
-	} else if (!strcmp(memory, "NANYA_NT5CC256M16ER-EK")) {
-		memory_config = NANYA_DDR3L_1866_512M;
-	} else if (!strcmp(memory, "HYNIX_H5TC4G63EFR-RDA")) {
-		memory_config = HYNIX_DDR3L_1866_512M;
-	} else {
-		lprintf(LOG_ERR, "Invalid memory configuration %s\n", memory);
-		goto exit;
-      }
 exit:
-	if (buf)
-		free(buf);
-	return memory_config;
-}
+	*info_out = info;
 
-static int get_mem_info(struct platform_intf *intf,
-			const struct nonspd_mem_info **info)
-{
-	switch (get_memory_config(intf)) {
-	case SAMSUNG_DDR3L_1866_512M:
-		*info = &samsung_k4b4g1646e_byma;
-		break;
-	case NANYA_DDR3L_1866_512M:
-		*info = &nanya_ddr3l_nt5cc256m16er_ek;
-		break;
-	case HYNIX_DDR3L_1866_512M:
-		*info = &hynix_ddr3l_h5tc4g63efr_rda;
-		break;
-	default:
+	if (!info) {
+		lprintf(LOG_ERR, "Unknown memory part number: %s", part_num);
 		return -1;
 	}
 
