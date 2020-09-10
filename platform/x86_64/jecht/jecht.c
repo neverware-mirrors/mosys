4db636de3de7f2dca3bcb60bbf0bf2bfec0a4670
Jack Rosenthal <jrosenth@chromium.org>
2020-09-10 11:05:41 -0600

    platform/x86_64: re-add support for Jecht devices
    Jecht baseboard devices used to be supported in the "beltino" platform
    in mosys, which was thought to be AUE since all devices from the
    beltino reference board went AUE.  In CL:2390887, the support for
    beltino was dropped.
    
    As it turns out, Jecht devices (Guado, Rikku, and Tidus) won't go AUE
    till next year, so we can't drop them yet.  A clean revert of the
    beltino drop would be difficult as many refactors have already landed
    which assumed the beltino platorm was deleted.  So, instead, this
    introduces a new "Jecht" platform supporting only those three devices.
    
    BUG=chromium:1126298
    TEST=run all mosys commands on guado, rikku, and tidus
    
    Change-Id: If2cefb7ba84b3570ddee1d9e61ba13778ead39a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/mosys/+/2404041
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Wei-Cheng Xiao <garryxiao@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

---
+++ platform/x86_64/jecht/jecht.c
@@ -0,0 +1,73 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <stddef.h>
+
+#include "lib/generic_callbacks.h"
+#include "lib/math.h"
+#include "lib/memory.h"
+#include "lib/probe.h"
+#include "lib/sku.h"
+#include "mosys/alloc.h"
+#include "mosys/command_list.h"
+#include "mosys/platform.h"
+
+static struct platform_cmd *jecht_sub[] = {
+	&cmd_eventlog,
+	&cmd_memory,
+	&cmd_platform,
+	&cmd_psu,
+	NULL,
+};
+
+static int jecht_probe(struct platform_intf *intf)
+{
+	static struct {
+		const char *smbios_name;
+		struct sku_info info;
+	} models[] = {
+		{ "Guado", { .model = "guado", .brand = "ASUG" } },
+		{ "Rikku", { .model = "rikku", .brand = "ACAQ" } },
+		{ "Tidus", { .model = "tidus", .brand = "LEAF" } },
+	};
+	int rv;
+
+	for (size_t i = 0; i < ARRAY_SIZE(models); i++) {
+		const char *probe_list[] = { models[i].smbios_name, NULL };
+
+		rv = probe_smbios(intf, probe_list);
+		if (rv < 0) {
+			return -1;
+		} else if (rv > 0) {
+			intf->sku_info = &models[i].info;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+static struct eventlog_cb jecht_eventlog_cb = {
+	.print_type = &elog_print_type,
+	.print_data = &elog_print_data,
+	.print_multi = &elog_print_multi,
+	.verify = &elog_verify,
+	.verify_header = &elog_verify_header,
+	.fetch = &elog_fetch_from_smbios,
+};
+
+static struct platform_cb jecht_cb = {
+	.memory = &smbios_memory_cb,
+	.psu = &generic_psu_ac_only_cb,
+	.eventlog = &jecht_eventlog_cb,
+};
+
+static struct platform_intf platform_jecht = {
+	.type = PLATFORM_X86_64,
+	.sub = jecht_sub,
+	.cb = &jecht_cb,
+	.probe = jecht_probe,
+};
+REGISTER_PLATFORM(platform_jecht, "Jecht");
